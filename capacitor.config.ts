13de83a0e673c6f2e304c5fd12ffab74d0678d15
M. Nindra Zaka <nindra.zaka@ruangguru.com>
2022-07-07 11:55:38 +0700

    add ios platform and camera feature

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,14 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'capasitor-basic',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "com.example.app",
+  appName: "capasitor-basic",
+  webDir: "build",
+  bundledWebRuntime: false,
+  server: {
+    url: "http://192.168.1.11:3000",
+    cleartext: true,
+  },
 };
 
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Capacitor } from "@capacitor/core";
+import { Camera, CameraResultType } from "@capacitor/camera";
 
 function App() {
+  const [imageUrl, setImageUrl] = React.useState<string | null>(null);
+
+  const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri,
+    });
+
+    setImageUrl(image.webPath ?? null);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img
+          src={imageUrl === null ? logo : imageUrl}
+          className="App-logo"
+          alt="logo"
+        />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -16,9 +34,16 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Capasitor.js
         </a>
+
+        <input type="text" />
+
+        <button onClick={takePicture}>Take picture</button>
       </header>
+      {Capacitor.isNativePlatform() && (
+        <p>Opened in {Capacitor.getPlatform()}</p>
+      )}
     </div>
   );
 }
